f4330714d8adb975e1883f5ae2657134ae7eab57
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-09-20 21:35:56 -0300

    update

--- ga.py
+++ ga.py
@@ -5,3 +5,9 @@ def fitness(equation_inputs, population):
 
 def selection(population, fitness, num_parents):
     parents = np.empty((num_parents, population.shape[1]))
+    
+    for idx in range(num_parents):
+        max_fitness_idx = np.where(fitness == np.max(fitness))
+        max_fitness_idx = max_fitness_idx[0][0]
+        
+        parents[idx, :] = population[max_fitness_idx, :]
